2fbefa6f310910924ed47abf10413ccc7261a787
Amany Kassem <amany.kassem95@gmail.com>
2024-04-29 08:05:37 +0200

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  // toggle menu
+  $("nav .toggle").click(function () {
+    $(".overlay").css({
+      transform: "scaleX(1)",
+    });
+
+    $(".menu").addClass("ul-dir");
+  });
+
+  $("nav .overlay").click(function () {
+    $(this).removeAttr("style");
+    $(".menu").removeClass("ul-dir");
+  });
+
+  // toggle fav
+  $(".toggle-fav").on("click", function () {
+    $(this).find(".fa-heart").toggleClass("fw-bold");
+  });
+
+  function initiateAnimation() {
+    AOS.init({
+      delay: 500, // values from 0 to 3000, with step 50ms
+      duration: 900, // values from 0 to 3000, with step 50ms
+      easing: "ease-out-back", // default easing for AOS animations
+    });
+
+    if ($(window).width() < 560) {
+      AOS.init({
+        once: true,
+      });
+    }
+  }
+  initiateAnimation();
+
+  //scroll top
+  var scrollButton = $("#scroll-top");
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 700) {
+      scrollButton.fadeIn(1000);
+    } else {
+      scrollButton.fadeOut(1000);
+    }
+  });
+
+  //click to scroll top
+  scrollButton.click(function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
